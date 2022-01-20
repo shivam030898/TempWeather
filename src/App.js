ce2721f4c263f29b8497eea03225bfa88dd82436
shivam030898 <s.shivam7879@gmail.com>
2022-01-20 17:46:26 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from'react';
+import Tempapp from './components/Tempapp';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Tempapp/>
+    </div>
+  );
+}
+
+export default App;
